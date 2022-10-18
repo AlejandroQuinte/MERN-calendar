86d05768dbb1e92a1c3aa15e030e9d5d6a0cf575
Alejandro <alequinteroz76@gmail.com>
2022-10-18 13:35:06 -0600

    Cambio en ruta bases

--- index.js
+++ index.js
@@ -25,8 +25,11 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
-//TODO: CRUD: Eventos
 
+//para que las rutas funcionen correctamente
+app.get('*',(req,res)=>{
+    res.sendFile(__dirname+ '/public/index.html');
+});
 
 //escuchar peticiones
 app.listen(process.env.PORT, () => {
